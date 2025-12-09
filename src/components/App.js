ed345b0b85af30ccd6dfc051e8d19be6196fa493
= <=>
2025-12-09 23:30:29 +0530

    Refresh

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Time on Page: {seconds} seconds</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
